a4cff75b7260c6f7c5244a9c88268569e7924713
gauenk <kent.gauen@gmail.com>
2021-10-24 14:41:46 -0400

    added py-gdb files

---
+++ init_test_paths.py
@@ -0,0 +1,3 @@
+# import sys
+# sys.path.append("./tests/kmburst/")
+from tests.kmburst.test_kmburst import test_burst_nnf as run
---
+++ py_gdb_shell.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PYTHONSTARTUP="./init_test_paths.py"
+python 
+export PYTHONSTARTUP=""
